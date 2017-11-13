9b926f66091f9f180ee33e5817e88a23c00ca085
Ngoc Pham <tokanizar@gmail.com>
2017-11-13 00:09:05 -0500

    Really got the code up

---
+++ functions.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Pool;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+/**
+ * Get top stories
+ *
+ * @param int $maxItems Max number of items to show
+ *
+ * @return void
+ */
+function getTopStories(int $maxItems = 30)
+{
+    $client = new Client();
+
+    $response = $client->get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    $topStories = json_decode($response->getBody());
+
+    $promise;
+    $stories = [];
+
+    $requests = function ($storyIds) {
+        foreach ($storyIds as $sId) {
+            yield new Request('GET', "https://hacker-news.firebaseio.com/v0/item/{$sId}.json");
+        }
+    };
+
+    $pool = new Pool($client, $requests(array_slice($topStories, 0, 100)), [
+        'concurrency' => 5,
+        'fulfilled' => function ($response, $index) use(&$stories, &$promise, $maxItems) {
+            // Resolve the promise if we have enough
+            if (count($stories) === $maxItems) {
+                $promise->resolve(true);
+                return;
+            }
+
+            $story = json_decode($response->getBody());
+
+            if ((isset($story->text) && $story->text !== '') || !isset($story->url)) return;
+
+            array_push($stories, $story);
+        },
+        'rejected' => function ($reason, $index) {
+            // this is delivered each failed request
+        },
+    ]);
+
+    // Initiate the transfers and create a promise
+    $promise = $pool->promise();
+
+    // Force the pool of requests to complete.
+    $promise->wait();
+
+    foreach ($stories as $story) {
+        $linkDomain = parse_url($story->url)['host'];
+
+        echo "<li data-sid='{$story->id}'>" .
+                "<a href='{$story->url}' target='_blank' rel='noopener noreferrer'>{$story->title}</a> " .
+                "<span>({$linkDomain})</span>" .
+                "<a href='https://news.ycombinator.com/item?id={$story->id}' target='_blank' rel='noopener noreferrer'> Discussion</a>" .
+            "</li>";
+    }
+}
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Quiet Hacker News</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="hacker news, hacker, news, quiet, quiet hacker news, tom speak, hn, hn without comments, hacker news without comments, hacker news no comments">
+    <meta name="HandheldFriendly" content="true" />
+    <meta name="Description" content="A quieter approach to Hacker News. Links and nothing else." />
+
+    <style>
+      html, body { background: #F0F0F0; }
+      body {
+        width: 800px;
+        margin: 44px auto;
+      }
+      ol {
+        list-style: ;
+        margin: 0 0 0 18px;
+        padding: 0;
+      }
+      li {
+        margin-bottom: 22px;
+      }
+      .links {
+        margin-top: 64px;
+        padding-bottom: 4px;
+      }
+      .links a {
+        float: left;
+      }
+      .links a:last-of-type {
+        float: right;
+      }
+
+      @media only screen and (max-device-width: 820px) {
+        body {
+          width: 85%;
+        }
+        h1 {
+          margin-bottom: 30px;
+          font-size: 40px;
+        }
+        li {
+          margin-bottom: 50px;
+          font-size: 20px;
+        }
+        li span {
+          display: block;
+        }
+        .links a {
+          font-size: 18px;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Quiet Hacker News</h1>
+
+    <ol>
+        <?php
+
+            require_once 'functions.php';
+
+            getTopStories();
+
+        ?>
+    </ol>
+
+    <div class="links">
+      <a href="http://github.com/tomspeak/quiet-hacker-news">Inspired By</a>
+    </div>
+
+  </body>
+</html>
