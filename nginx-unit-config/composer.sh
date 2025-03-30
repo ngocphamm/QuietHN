7992c9387a696a4f4d29b6989f4e2e9144a13cf1
Ngoc Pham <ngocphamm@fastmail.fm>
2025-03-30 15:08:48 -0400

    Well forgot the malformed SHA256

--- nginx-unit-config/composer.sh
+++ nginx-unit-config/composer.sh
@@ -8,7 +8,7 @@ cd /www
 # https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
 curl https://getcomposer.org/download/2.8.6/composer.phar --output composer.phar
 
-echo 'becc28b909d2cca563e7caee1e488063312af36b1f2e31db64f417723b8c402 composer.phar' | sha256sum --check
+echo 'becc28b909d2cca563e7caee1e488063312af36b1f2e31db64f417723b8c4026 composer.phar' | sha256sum --check
 
 RESULT=$?
 
